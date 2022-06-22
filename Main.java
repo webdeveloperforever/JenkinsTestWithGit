62e6ca86f15c8eee60464eee4d84351e79f1b5b2
webdeveloperforever <yemulasaisatya291@gmail.com>
2022-06-22 20:45:47 +0530

    done

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String[] args){
         System.out.println("Test with jenkins using git");
+        System.out.println("Sucessfully connected to ngork for the auto pulling of the code from the git");
     }
 }
